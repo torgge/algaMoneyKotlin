d83281ef968e14c1b162737125c1a865c33d36a6
George Bonespírito <torgge@gmail.com>
2017-08-03 07:25:31 -0300

    Aula 3.9 - Corrigindo bug de anotação.

--- src/main/kotlin/com/bonespirito/kotlinflyway/exceptionhandler/AlgamoneyExceptionHandler.kt
+++ src/main/kotlin/com/bonespirito/kotlinflyway/exceptionhandler/AlgamoneyExceptionHandler.kt
@@ -7,10 +7,11 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
-
+@ControllerAdvice
 open class AlgamoneyExceptionHandler: ResponseEntityExceptionHandler() {
 
     @Autowired
