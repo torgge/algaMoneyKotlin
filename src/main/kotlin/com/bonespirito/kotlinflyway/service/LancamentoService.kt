6a5c80667e640603391eebe647d319416eea58dd
George Bonespírito <torgge@gmail.com>
2017-11-25 14:31:00 -0200

    -Refatoração do Service. -Atualização do Kotlin.

--- src/main/kotlin/com/bonespirito/kotlinflyway/service/LancamentoService.kt
+++ src/main/kotlin/com/bonespirito/kotlinflyway/service/LancamentoService.kt
@@ -6,26 +6,19 @@ import com.bonespirito.kotlinflyway.repository.LancamentoRepository
 import com.bonespirito.kotlinflyway.repository.PessoaRepository
 import com.bonespirito.kotlinflyway.service.exception.PessoaInexistenteOuInativaException
 import org.springframework.beans.BeanUtils
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 
 @Service
-class LancamentoService {
-
-    @Autowired
-    val lancamentoRepository: LancamentoRepository? = null
-
-    @Autowired
-    val pessoaRepository: PessoaRepository? = null
+class LancamentoService(val lancamentoRepository: LancamentoRepository, val pessoaRepository: PessoaRepository) {
 
     fun salvar(lancamento: Lancamento): Lancamento {
 
-        val pessoaDoLancamento = pessoaRepository?.findOne(lancamento.pessoa?.codigo)
+        val pessoaDoLancamento = pessoaRepository.findOne(lancamento.pessoa?.codigo)
         if (pessoaDoLancamento == null || pessoaDoLancamento.isInativo())
             throw PessoaInexistenteOuInativaException()
 
-            return lancamentoRepository?.save(lancamento)!!
+        return lancamentoRepository.save(lancamento)!!
     }
 
     fun atualizar(codigo: Long?, lancamento: Lancamento): Lancamento? {
@@ -35,23 +28,21 @@ class LancamentoService {
         }
 
         BeanUtils.copyProperties(lancamento, lancamentoSalvo, "codigo")
-
-        var lancamentoRepo = lancamentoRepository
-        return if (lancamentoRepo != null) lancamentoRepo.save(lancamentoSalvo) else null
+        return lancamentoRepository.save(lancamentoSalvo)
     }
 
-    private fun validarPessoa(lancamento : Lancamento) {
+    private fun validarPessoa(lancamento: Lancamento) {
         var pessoa: Pessoa? = null
         if (lancamento.pessoa?.codigo != null) {
-            pessoa = pessoaRepository?.findOne(lancamento.pessoa?.codigo)
+            pessoa = pessoaRepository.findOne(lancamento.pessoa?.codigo)
         }
 
         if (pessoa == null || pessoa.isInativo()) {
             throw PessoaInexistenteOuInativaException()
         }
-	}
+    }
 
     private fun buscarLancamentoExistente(codigo: Long?): Lancamento {
-        return lancamentoRepository?.findOne(codigo) ?: throw IllegalArgumentException()
+        return lancamentoRepository.findOne(codigo) ?: throw IllegalArgumentException()
     }
 }
--- src/main/kotlin/com/bonespirito/kotlinflyway/service/PessoaService.kt
+++ src/main/kotlin/com/bonespirito/kotlinflyway/service/PessoaService.kt
@@ -3,30 +3,26 @@ package com.bonespirito.kotlinflyway.service
 import com.bonespirito.kotlinflyway.model.Pessoa
 import com.bonespirito.kotlinflyway.repository.PessoaRepository
 import org.springframework.beans.BeanUtils
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.stereotype.Service
 
 @Service
-class PessoaService {
-
-    @Autowired
-    var pessoaRepository: PessoaRepository? = null
+class PessoaService(val pessoaRepository: PessoaRepository) {
 
     fun atualizar(codigo: Long, pessoa: Pessoa): Pessoa {
         val pessoaSalva = buscarPessoaPeloCodigo(codigo)
         BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo")
-        return pessoaRepository?.save(pessoaSalva)!!
+        return pessoaRepository.save(pessoaSalva)!!
     }
 
     fun atualizarPropriedadeAtivo(codigo: Long, ativo: Boolean) {
         val pessoaSalva = buscarPessoaPeloCodigo(codigo)
         pessoaSalva.ativo = ativo
-        pessoaRepository?.save(pessoaSalva)
+        pessoaRepository.save(pessoaSalva)
     }
 
     private fun buscarPessoaPeloCodigo(codigo: Long): Pessoa {
-        val pessoaSalva = pessoaRepository?.findOne(codigo) ?: throw EmptyResultDataAccessException(1)
+        val pessoaSalva = pessoaRepository.findOne(codigo) ?: throw EmptyResultDataAccessException(1)
         return pessoaSalva
     }
 }
